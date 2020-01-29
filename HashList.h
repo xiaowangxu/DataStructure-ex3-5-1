08b2c3616011381c70d91e5061ff401705fcd615
shu-ssyze <46869158+shu-ssyze@users.noreply.github.com>
2020-01-29 12:27:27 +0800

    task1
    no debug

--- HashList.h
+++ HashList.h
@@ -1,92 +1,137 @@
 #pragma once
 
-namespace TwiceCheckHashList
-{
-enum Result
-{
-	OK,
-	FOUND,
-	NOT_FOUND,
-	OUT_OF_BOUNDRY,
-	BAD_HASH_FUNCTION
+namespace TwiceCheckHashList {
+enum Result {
+    OK,
+    FOUND,
+    NOT_FOUND,
+    OUT_OF_BOUNDRY,
+    BAD_HASH_FUNCTION
 };
 
 template <class DataType>
-class Item
-{
+class Item {
 public:
-	DataType data;
-	bool has_data = false;
+    DataType data;
+    bool has_data = false;
 
 public:
-	Item();
-	Item(const DataType &Data);
+    Item();
+    Item(const DataType& Data);
 };
 
 template <class DataType>
 Item<DataType>::Item()
 {
-	this->has_data = false;
+    this->has_data = false;
 }
 
 template <class DataType>
-Item<DataType>::Item(const DataType &Data)
+Item<DataType>::Item(const DataType& Data)
 {
-	this->data = Data;
-	this->has_data = true;
+    this->data = Data;
+    this->has_data = true;
 }
 
 template <class DataType>
-class HashList
-{
+class HashList {
 private:
-	Item<DataType> *list = nullptr;
-	int max_size = 0;
-	int (*hash_func)(DataType &Data);
+    Item<DataType>* list = nullptr;
+    int max_size = 0;
+    int (*hash_func)(DataType& Data);
+    int num = 0;
 
 public:
-	HashList(const int Max, int (*HashFunction)(DataType &Data));
-	~HashList();
+    HashList(const int Max, int (*HashFunction)(DataType& Data));
+    ~HashList();
+    void Check();
 
-	Result insert(DataType &Data);
-	Result remove(DataType &Data);
-	Result search(DataType &Data);
+    Result insert(DataType& Data);
+    Result remove(DataType& Data);
+    Result search(DataType& Data, int& p);
 };
 
 template <class DataType>
-HashList<DataType>::HashList(const int Max, int (*HashFunction)(DataType &Data))
+HashList<DataType>::HashList(const int Max, int (*HashFunction)(DataType& Data))
 {
-	this->max_size = Max;
-	this->hash_func = HashFunction;
-	this->list = new Item<DataType>[this->max_size];
-	if (this->list == nullptr)
-	{
-		this->max_size = 0;
-	}
+    this->max_size = Max;
+    this->hash_func = HashFunction;
+    this->list = new Item<DataType>[this->max_size];
+    if (this->list == nullptr) {
+        this->max_size = 0;
+    }
 }
 
 template <class DataType>
 HashList<DataType>::~HashList()
 {
-	if (this->list != nullptr)
-	{
-		delete[] this->list;
-	}
+    if (this->list != nullptr) {
+        delete[] this->list;
+    }
 }
 
 template <class DataType>
-Result HashList<DataType>::insert(DataType &Data)
+void HashList<DataType>::Check()
 {
+    if (num * 2 >= max_size) {
+        delete[] this - list;
+        max_size = (max_size + 1) * 2;
+        this->list = new Item<DataType>[this->max_size];
+    }
 }
 
 template <class DataType>
-Result HashList<DataType>::remove(DataType &Data)
+Result HashList<DataType>::insert(DataType& Data)
 {
+    int pos = (*hash_func)(Data);
+    if (pos >= max_size)
+        return BAD_HASH_FUNCTION;
+    Check();
+    num++;
+    int i = 1, next_pos = pos;
+    while (list[next_pos].has_data) {
+        next_pos = (pos + i + max_size) % max_size;
+        if (!list[next_pos].has_data)
+            break;
+        next_pos = (pos + i + max_size) % max_size;
+        i *= 2;
+    }
+    list[next_pos].has_data = true;
+    list[next_pos].data = Data;
+    return OK;
 }
 
 template <class DataType>
-Result HashList<DataType>::search(DataType &Data)
+Result HashList<DataType>::remove(DataType& Data)
 {
+    int pos;
+    if (search(Data, pos) == NOT_FOUND)
+        return NOT_FOUND;
+    list[pos].has_data = false;
+    return OK;
+}
+
+template <class DataType>
+Result HashList<DataType>::search(DataType& Data, int& p)
+{
+    int pos = (*hash_func)(Data);
+    if (pos >= max_size)
+        return NOT_FOUND;
+    int i = 1, next_pos = pos;
+    while (list[next_pos].has_data && i <= max_size) {
+        if (list[next_pos].data == Data) {
+            p = next_pos;
+            return FOUND;
+        }
+        next_pos = (pos + i + max_size) % max_size;
+        if (list[next_pos].data == Data) {
+            p = next_pos;
+            return FOUND;
+        }
+        next_pos = (pos - i + max_size) % max_size;
+        i *= 2;
+    }
+    return NOT_FOUND;
 }
 
 }; // namespace TwiceCheckHashList
--- ex3-5-1.cpp
+++ ex3-5-1.cpp
@@ -1,18 +1,28 @@
-﻿// ex3-5-1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+﻿/*
+ * @Date         : 2020-01-28 00:51:20
+ * @Author       : ssyze
+ * @Description  : 
+ */
+// ex3-5-1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
-#include <iostream>
 #include "HashList.h"
+#include <iostream>
 using namespace TwiceCheckHashList;
 
-int HashFunc(char &Data)
+int HashFunc(char& Data)
 {
-	return Data % 100;
+    return Data % 100;
 }
 
 int main()
 {
-	HashList<char> test(100, HashFunc);
+    HashList<char> test(100, HashFunc);
+    std::cout << OK << std::endl;
+    int pos = 0;
+    char a = 'a';
+    int res = test.search(a, pos);
+    std::cout << res << std::endl;
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
